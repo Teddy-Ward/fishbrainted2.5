002320d92f5e8b5a013cee6afce12c575dc577e9
Ted Ward <teddyward@gmail.com>
2023-08-23 13:00:47 +0100

    blog post not found added

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,50 +1,59 @@
 import { useParams } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 import { useState, useEffect } from "react";
-import PostCard from './PostCard';
+import PostCard from "./PostCard";
 
 import Loading from "../Loading";
 
-const CDN = import.meta.env.VITE_SUPABASE_IMAGES_CDN
+const CDN = import.meta.env.VITE_SUPABASE_IMAGES_CDN;
 
-
-export default function BlogPost({params}) {
+export default function BlogPost({ params }) {
   let { slug } = useParams();
   const [post, setPost] = useState([""]);
-  const [loading, setLoading] = useState("block")
-  const [classes, setClasses] = useState("hidden")
+  const [loading, setLoading] = useState("block");
+  const [classes, setClasses] = useState("hidden");
 
   useEffect(() => {
     getPost();
-  },[] );
+  }, []);
 
   async function getPost() {
     try {
-      const { data, error } = (await supabase.from("blog").select("*").eq("slug_title", slug));
-      if (error) throw error;      
+      const { data, error } = await supabase
+        .from("blog")
+        .select("*")
+        .eq("slug_title", slug);
+      if (error) throw error;
       if (data != null) {
         setPost(data);
-      } 
-
+      }
     } catch (error) {
-      alert(error.message); 
+      alert(error.message);
     } finally {
-      setLoading("hidden")
-      setClasses("block")
+      setLoading("hidden");
+      setClasses("block");
     }
   }
 
-  console.log(post)
-
-
-
-
+  console.log(post);
 
   return (
-  <>
-  {/* <div>Now showing post {slug}</div> */}
+    <>
+      {/* <div>Now showing post {slug}</div> */}
 
-      {post[0] ? (<PostCard post={post[0]} /> ) : (<Loading /> ) }
-
-  </>)
+      {post[0] ? (
+        <PostCard post={post[0]} />
+      ) : (
+        <div className="pt-20">
+          <div className={loading}>
+            <Loading />
+          </div>
+          <div className={classes}>
+            <h2>Page Not Found</h2>
+            <a href={"/"}>Go Back</a>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
